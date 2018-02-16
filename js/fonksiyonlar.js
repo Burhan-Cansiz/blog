ba13286b124c66144489be5239821f65e864440b
Burhan <burhancansiz@outlook.com>
2018-02-17 02:32:42 +0300

    .

---
+++ js/fonksiyonlar.js
@@ -0,0 +1,17 @@
+$(function () {
+
+    $.aramaFormAc = function () {
+        $('.aramaForm').show();
+    }
+
+    $.aramaFormKapat = function () {
+        $('.aramaForm').hide();
+    } 
+
+
+
+
+
+});
+
+
